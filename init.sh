b5135f2531a5d6f28a10acd90ea31e04312bd79d
Khanze99 <khamidov_anvar@mail.ru>
2020-01-26 23:16:20 +0300

    modified init.sh and config server

--- init.sh
+++ init.sh
@@ -3,3 +3,5 @@ echo "REMOVE default config"
 sudo cp ./etc/server.conf /etc/nginx/sites-enabled/
 echo "COPY CONFIG"
 sudo service nginx reload
+echo "NGINX RELOADED"
+gunicorn --bind=0.0.0.0:8080 hello:web_application
