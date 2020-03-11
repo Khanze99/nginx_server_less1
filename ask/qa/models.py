4f32c509cf7394333ee6e6acef9043135c3551d8
Khanze99 <khamidov_anvar@mail.ru>
2020-03-11 13:48:18 +0000

    change migrations2

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -23,7 +23,7 @@ class Question(models.Model):
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateTimeField(auto_now_add=True)
+    added_at = models.DateTimeField(auto_now_add=True, null=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
