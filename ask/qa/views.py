b309adfe7eff41069909e4221c4bfc1b37bad4e2
Khanze99 <khamidov_anvar@mail.ru>
2020-03-11 10:41:42 +0300

    add view question_list

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator
+
+from .models import Question
 # Create your views here.
 
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
+
+
+def question_list(request):
+    page_number = request.GET.get('page', 1)
+    limit = 10
+    objects = Question.objects.new()
+    paginator = Paginator(objects, limit)
+    page_object = paginator.get_page(page_number)
+
+    
